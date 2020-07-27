a1428bb3f11a4a4f4352b69aeebd000b92254e51
paddyjclancy <patrickclancy0605@gmail.com>
2020-07-27 16:31:59 +0100

    VPC created, db instance

--- init.sh.tpl
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /home/ubuntu/web-app
-npm install
-pm2 start app.js
--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "aws_vpc" "patrick_vpc" {
 resource "aws_internet_gateway" "IGW" {
   vpc_id = aws_vpc.patrick_vpc.id
   tags = {
-    Name = "Eng57.Patrick.TF.IGW"
+    Name = "${var.name}TF.IGW"
   }
 }
 
@@ -37,7 +37,7 @@ resource "aws_subnet" "Public-sub" {
   availability_zone = "eu-west-1b"
   map_public_ip_on_launch = true
   tags = {
-    Name = "Eng57.Patrick.C.TF.PublicSubnet"
+    Name = "${var.name}TF.PublicSubnet"
   }
 }
 
@@ -70,12 +70,11 @@ resource "aws_security_group" "SG-web" {
   }
 
   tags = {
-    Name = "Eng57.Patrick.C.SG.App"
+    Name = "${var.name}SG.App"
   }
 }
 
 # NACL - Public
-
 resource "aws_network_acl" "public-nacl" {
   vpc_id = aws_vpc.patrick_vpc.id
 
@@ -122,11 +121,11 @@ resource "aws_network_acl" "public-nacl" {
   subnet_ids = [aws_subnet.Public-sub.id]
 
     tags = {
-      Name = "Eng57.Patrick.C.NACL.public"
+      Name = "${var.name}NACL.public"
     }
 }
 
-# Route Table
+# Route Table - Public
 resource "aws_route_table" "route-public" {
   vpc_id = aws_vpc.patrick_vpc.id
 
@@ -134,8 +133,9 @@ resource "aws_route_table" "route-public" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.IGW.id
   }
+
   tags = {
-    Name = "Eng57.Patrick.C.Route.Public"
+    Name = "${var.name}Route.Public"
   }
 }  
 
@@ -150,7 +150,7 @@ data "template_file" "initapp" {
   template = file("./scripts/app/init.sh.tpl")
 }
 
-# Create EC2 instance IMAGE with app
+# Create EC2 instance IMAGE with APP
 resource "aws_instance" "Web" {
   ami                         = "ami-00b48f09c568b0014"
   instance_type               = "t2.micro"
@@ -159,8 +159,153 @@ resource "aws_instance" "Web" {
   associate_public_ip_address = true
   user_data = data.template_file.initapp.rendered
   tags = {
-    Name = "Eng57.Patrick.C.TF.App"
+    Name = "${var.name}TF.App"
   }
   
 }
 
+# Private Subnet
+resource "aws_subnet" "Private-sub" {
+  vpc_id = aws_vpc.patrick_vpc.id
+  cidr_block = "16.0.2.0/24"
+  availability_zone = "eu-west-1b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${var.name}TF.PrivateSubnet"
+  }
+}
+
+
+# Security Group - Private
+resource "aws_security_group" "SG-db" {
+  name = "SG-db"
+  description = "Allow http and MongoDB traffic"
+  vpc_id = aws_vpc.patrick_vpc.id 
+
+  ingress {
+    description = "Mongodb from VPC"
+    from_port   = 27017
+    to_port     = 27017
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "http from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.name}TF.SG.db"
+  }
+}
+
+# NACL - Private
+resource "aws_network_acl" "private-nacl" {
+  vpc_id = aws_vpc.patrick_vpc.id
+
+  # Port 27017
+
+   ingress {
+     protocol = "tcp"
+     rule_no = 100
+     action = "allow"
+     cidr_block = "16.0.1.0/24"
+     from_port = 27017
+     to_port = 27017
+   }
+
+  # Ephemeral Ports
+
+  egress {
+    protocol = "tcp"
+    rule_no = 100
+    action = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port = 1024
+    to_port = 65535
+  }
+
+  ingress {
+    protocol = "tcp"
+    rule_no = 110
+    action = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port = 1024
+    to_port = 65535
+  }
+
+  # Inbounds - 80, 443
+
+  egress {
+    protocol = "tcp"
+    rule_no = 110
+    action = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port = 80
+    to_port = 80
+  }
+
+  egress {
+    protocol = "tcp"
+    rule_no = 120
+    action = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port = 443
+    to_port = 443
+  }
+
+  subnet_ids = [aws_subnet.Private-sub.id]
+
+    tags = {
+      Name = "${var.name}NACL.TF.private"
+    }
+}
+
+# Create EC2 instance for DB
+resource "aws_instance" "DB" {
+  ami                         = "ami-089cc16f7f08c4457"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.Private-sub.id
+  vpc_security_group_ids      = [aws_security_group.SG-db.id]
+  associate_public_ip_address = true
+  user_data = data.template_file.initapp.rendered
+  tags = {
+    Name = "${var.name}TF.db"
+  }
+  
+}
+
+# Route Table - Private
+resource "aws_route_table" "route-private" {
+  vpc_id = aws_vpc.patrick_vpc.id
+
+  # route {
+  #   cidr_block = "16.0.0.0/16"
+  #   instance_id = aws_instance.DB.id
+  # }
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.IGW.id
+  }
+
+  tags = {
+    Name = "${var.name}Route.TF.Private"
+  }
+}  
+
+# Route table associations
+resource "aws_route_table_association" "route-db" {
+  subnet_id = aws_subnet.Private-sub.id
+  route_table_id = aws_route_table.route-private.id
+}
